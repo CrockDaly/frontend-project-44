984a68ae14d491a87507b4da4b534fd8a69e3aa0
Matvey Banin <baninmatvey333@gmail.com>
2023-07-02 17:17:41 +1000

    add eslint modules and add new makefile eslint conf

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,5 @@
 import readlineSync from 'readline-sync';
+
 export function name() {
-    console.log('Hello, ' + readlineSync.question('May I have your name? ') + '!');
-};
+    console.log(`Hello, ${readlineSync.question('May I have your name? ')}!`);
+}
