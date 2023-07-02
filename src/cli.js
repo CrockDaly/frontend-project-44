2f20b81233898e357115e9e0f43e10a5764f4197
Matvey Banin <baninmatvey333@gmail.com>
2023-07-02 17:23:21 +1000

    fixes syntax errors

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,6 @@
+/* eslint-disable indent */
 import readlineSync from 'readline-sync';
 
-export function name() {
+export default function name() {
     console.log(`Hello, ${readlineSync.question('May I have your name? ')}!`);
 }
